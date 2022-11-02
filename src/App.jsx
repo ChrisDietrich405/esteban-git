6102fea52e033ff818319dbe2d9c3eb08e8283f8
Chris <chrisdietrich3666@gmail.com>
2022-11-02 18:34:26 -0500

    feat added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  return <h1>Hello Esteban, <br></br>
+  return ( 
+  <>
+  <h1>Hello Esteban, <br></br>
   Hello, Chris :)
-  </h1>;
+  </h1>
+  <sidebar>This is a sidebar</sidebar>
+  </>
+  )
 }
 
 export default App;
